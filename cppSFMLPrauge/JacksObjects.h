bc37c1c52605180046e32fbac1a5eed89e0c4870
jacksonlevine <jack@netbug.us>
2023-02-12 14:22:41 -0800

    add project

---
+++ cppSFMLPrauge/JacksObjects.h
@@ -0,0 +1,231 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+#include "SFML/Graphics.hpp"
+
+namespace objs {
+
+  int clamp(int val, int min, int max);
+
+	class ColorBrick {
+	public:
+		sf::Color col;
+		float elevation;
+    ColorBrick(sf::Color c, float elev) {
+      col = c;
+      elevation = elev;
+    };
+	};
+  
+  class ObjectBrick {
+  public:
+    sf::Color col;
+    int obx = 0;
+    int oby = 0;
+    ObjectBrick() {
+
+    };
+    ObjectBrick(sf::Color c, int ox, int oy) {
+      obx = ox;
+      oby = oy;
+      col = c;
+    };
+  };
+
+  class FixedObject {
+  public:
+    int width = 0;
+    int height = 0;
+    int x = 0;
+    int y = 0;
+    std::string thing;
+  };
+
+  class PlayerPixel {
+  public:
+    sf::Color col;
+    int px = 0;
+    int py = 0;
+    PlayerPixel() {
+
+    };
+    PlayerPixel(sf::Color c, int x, int y) {
+      col = c;
+      px = x;
+      py = y;
+    };
+  };
+
+	class Tree {
+	public:
+		static std::string makeTree() {
+      std::vector<int> intString;
+      int width = 26;
+      int height = 25;
+      int amplitudeX = 3;
+      for (int j = 0; j < width; j++) {
+        for (int i = 0; i < height; i++) {
+          intString.push_back(0);
+        }
+      }
+      sf::Vector2f initialSpot((int)(width / 2), height - 1);
+
+      sf::Vector2f initialDirection(((float)std::rand() / RAND_MAX) - 0.5, 1);
+
+      int trunkheight = std::min(((float)std::rand() / RAND_MAX) * 10, (float)5);
+      
+      sf::Vector2f nextSpot(0.0, 0.0);
+      for (int i = 0; i < trunkheight; i++) {
+        intString[clamp(((int)(initialSpot.y) * width) + (int)(initialSpot.x), 0, intString.size()-1)] = 1;
+        initialSpot.x += initialDirection.x;
+        initialSpot.y -= initialDirection.y;
+        nextSpot.x = initialSpot.x;
+        nextSpot.y = initialSpot.y;
+      }
+      
+      std::vector<sf::Vector2f> nextSpots;
+      for (int i = 0; i < 3; i++) {
+        int branchlength = std::max(((float)std::rand() / RAND_MAX) * 5, (float)3);
+        sf::Vector2f nextSpot2(0.0, 0.0);
+        sf::Vector2f newDirection((((float)std::rand() / RAND_MAX)-0.5)*amplitudeX, 1);
+        for (int b = 0; b < branchlength; b++) {
+          if (((int)(nextSpot.y) * width) + (int)(nextSpot.x) > 0 && ((int)(nextSpot.y) * width) + (int)(nextSpot.x) < (width * height) - 1) {
+            intString[clamp(((int)(nextSpot.y) * width) + (int)(nextSpot.x), 0, intString.size() - 1)] = 1;
+          }
+          nextSpot.x += newDirection.x;
+          nextSpot.y -= newDirection.y;
+          nextSpot2.x = initialSpot.x;
+          nextSpot2.y = initialSpot.y;
+        }
+        nextSpots.push_back(nextSpot2);
+      }
+      
+      std::vector<sf::Vector2f> nextSpotsClone = nextSpots;
+      std::vector<sf::Vector2f> leafspots;
+      
+      for (int i = 0; i < 3; i++) {
+        for (int t = 0; t < 3; t++) {
+          int finlength = std::max(((float)std::rand() / RAND_MAX) * 5, (float)3);
+          sf::Vector2f newDirection((((float)std::rand() / RAND_MAX)-0.5)*(float)amplitudeX, 1);
+          sf::Vector2f endOfThisBranch(0, 0);
+          for (int b = 0; b < finlength; b++) {
+            if (((int)std::floor(nextSpots[i].y) * width) + (int)std::floor(nextSpots[i].x) > 0 && ((int)std::floor(nextSpots[i].y) * width) + (int)std::floor(nextSpots[i].x) < (width * height) - 1) {
+              intString[clamp(((int)std::floor(nextSpots[i].y) * width) + (int)std::floor(nextSpots[i].x), 0, intString.size() - 1)] = 1;
+            
+            nextSpots[i].x += newDirection.x;
+            nextSpots[i].y -= newDirection.y;
+            endOfThisBranch.x = nextSpots[i].x;
+            endOfThisBranch.y = nextSpots[i].y;
+            }
+          }
+          if ((endOfThisBranch.y * width) + endOfThisBranch.x > 0 && (endOfThisBranch.y * width) + endOfThisBranch.x < (width * height) - 1) {
+            leafspots.push_back(endOfThisBranch);
+          }
+        }
+        for (int n = 0; n < nextSpots.size(); n++) {
+          nextSpots[n] = nextSpotsClone[n];
+        }
+      }
+      
+      for (sf::Vector2f i : leafspots) {
+        if (((int)(i.y + 1) * width) + (int)(i.x) > 0 && ((int)(i.y + 1) * width) + (int)(i.x) < (width * height) - 1) {
+          intString[clamp(((int)(i.y + 1) * width) + (int)(i.x), 0, intString.size() - 1)] = 3;
+        }
+        if (((int)(i.y + 1) * width) + (int)(i.x - 1) > 0 && ((int)(i.y + 1) * width) + (int)(i.x - 1) < (width * height) - 1) {
+          intString[clamp(((int)(i.y + 1) * width) + (int)(i.x - 1), 0, intString.size() - 1)] = 3;
+        }
+        if (((int)(i.y + 1) * width) + (int)(i.x + 1) > 0 && ((int)(i.y + 1) * width) + (int)(i.x + 1) < (width * height) - 1) {
+          intString[clamp(((int)(i.y + 1) * width) + (int)(i.x + 1), 0, intString.size() - 1)] = 3;
+        }
+        if (((int)(i.y) * width) + (int)(i.x - 1) > 0 && ((int)(i.y) * width) + (int)(i.x - 1) < (width * height) - 1) {
+          intString[clamp(((int)(i.y) * width) + (int)(i.x - 1), 0, intString.size() - 1)] = 2;
+        }
+        if (((int)(i.y) * width) + (int)(i.x + 1) > 0 && ((int)(i.y) * width) + (int)(i.x + 1) < (width * height) - 1) {
+          intString[clamp(((int)(i.y) * width) + (int)(i.x + 1), 0, intString.size() - 1)] = 2;
+        }
+        if (((int)(i.y) * width) + (int)(i.x - 2) > 0 && ((int)(i.y) * width) + (int)(i.x - 2) < (width * height) - 1) {
+          intString[clamp(((int)(i.y) * width) + (int)(i.x - 2), 0, intString.size() - 1)] = 2;
+        }
+        if (((int)(i.y) * width) + (int)(i.x + 2) > 0 && ((int)(i.y) * width) + (int)(i.x + 2) < (width * height) - 1) {
+          intString[clamp(((int)(i.y) * width) + (int)(i.x + 2), 0, intString.size() - 1)] = 2;
+        }
+      }
+
+      std::string string = "";
+
+      for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+          if (intString[clamp((j * width) + i, 0, intString.size()-1)] == 0) {
+            string = string + '0';
+          }
+          if (intString[clamp((j * width) + i, 0, intString.size()-1)] == 1) {
+            string = string + 't';
+          }
+          if (intString[clamp((j * width) + i, 0, intString.size()-1)] == 2) {
+            string = string + 'l';
+          }
+          if (intString[clamp((j * width) + i, 0, intString.size()-1)] == 3) {
+            string = string+ 'b';
+          }
+        }
+      }
+      return string;
+		}
+	};
+
+  int clamp(int val, int min, int max) {
+    return std::min(std::max(val, min), max);
+  }
+
+	class Player{
+		std::string name = "";
+		public: 
+			float x = 0;
+			float y = 0;
+			int width = 3;
+			int height = 5;
+			bool jump = false;
+			float elevation = 0;
+			float gravityForce = 0;
+			int jumpForce = 10;
+
+		Player(std::string pname, int px, int py) {
+			name = pname;
+			x = px;
+			y = py;
+		};
+
+		Player(std::string pname, int px, int py, int wid, int hei) {
+			name = pname;
+			x = px;
+			y = py;
+			width = wid;
+			height = hei;
+		};
+
+		std::string posKey() {
+			return "" + std::to_string((int)(x)) + ',' + std::to_string((int)(y));
+		}
+
+		void move(float mx, float my, std::unordered_map<std::string, Player*>* pmap) {
+			pmap->erase(posKey());
+			x += mx;
+			y += my;
+			pmap->insert_or_assign(posKey(), this);
+		}
+
+		void stepJump() {
+			if (jump == true) {
+				if (elevation >= 0) {
+					elevation += (jumpForce - gravityForce)/2;
+					gravityForce+=.4;
+				}
+				else {
+					jump = false;
+					gravityForce = 0;
+					elevation = 0;
+				}
+			}
+		}
+	};
+}
---
+++ cppSFMLPrauge/Source.cpp
@@ -0,0 +1,320 @@
+#include "SFML/Graphics/RenderStates.hpp";
+#include "perlin.h"
+#include "perlin.cpp"
+#include "JacksObjects.h"
+#include <iostream>
+sf::RenderWindow window(sf::VideoMode(1280, 720), "Window");
+std::unordered_map<std::string, objs::ColorBrick> worldmap;
+std::unordered_map<std::string, objs::FixedObject> fomap;
+std::unordered_map<std::string, objs::ObjectBrick> opixmap;
+std::unordered_map<std::string, objs::Player*> pmap;
+std::unordered_map<char, sf::Color> opixref;
+std::vector<std::string> mappedfos;
+void render(perlin p);
+void renderMinimap(int width, int x, int y, objs::Player*pla);
+void handleEvents(objs::Player* pla);
+void generateWorld(std::unordered_map<std::string, objs::ColorBrick>* wmap, perlin p);
+void insertIntoWorld(int x, int y, float red, float green, float blue, float a, float elevation, std::unordered_map<std::string, objs::ColorBrick>* wmap, objs::ColorBrick brick);
+
+float camX = 0;
+float camY = 0;
+
+int ts = 11;
+
+int width;
+int height;
+
+objs::Player play("Player1", (window.getSize().x / ts) / 2, (window.getSize().y / ts) / 2);
+int main() 
+{
+	sf::Color ocols;
+	ocols.r = 52;
+	ocols.g = 26;
+	ocols.b = 12;
+	ocols.a = 255;
+	opixref['t'] = ocols;
+
+	ocols.r = 40;
+	ocols.g = 120;
+	ocols.b = 35;
+	ocols.a = 255;
+	opixref['l'] = ocols;
+	ocols.r = 36;
+	ocols.g = 100;
+	ocols.b = 26;
+	ocols.a = 255;
+	opixref['b'] = ocols;
+
+	perlin p;
+	sf::Event e;
+
+	sf::Vector2u size = window.getSize();
+	width = size.x / ts;
+	height = size.y / ts;
+	
+	pmap[play.posKey()] = &play;
+
+	generateWorld(&worldmap, p);
+	while (window.isOpen())
+	{
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed) 
+			{
+				window.close();
+			}
+		}
+		window.clear(sf::Color::Black);
+		render(p);
+		renderMinimap(17, 90, 40, &play);
+		handleEvents(&play);
+		window.display();
+	}
+	
+	return 0;
+}
+bool test = false;
+
+double perlz = 0;
+
+sf::RectangleShape rect(sf::Vector2f(ts, ts));
+
+void render(perlin p) {
+	std::unordered_map<std::string, objs::PlayerPixel> screenumap;
+	double currOffsetX = 0.0;
+	double currOffsetY = 0.0;
+	int playSpotX = 0;
+	int playSpotY = 0;
+	for (int j = height+camY+1; j > 0+camY-1; j--) 
+	{
+		for (int i = 0+camX-1; i < width+camX+1; i++) 
+		{
+			int floorX = std::floor(i);
+			int floorY = std::floor(j);
+			std::string keySpot = "" + std::to_string(floorX) + ',' + std::to_string(floorY);
+			if (pmap.find(keySpot) != pmap.end())
+			{
+				int yoff = 0;
+				int yshrink = 0;
+				if (worldmap.find(keySpot) != worldmap.end()) {
+					yoff = std::floor(worldmap.at(keySpot).elevation);
+				}
+				else {
+					yshrink = 2;
+				}
+				int elevOs = std::floor(pmap.at(keySpot)->elevation / 5);
+				if (pmap.at(keySpot)->jump == true) {
+					pmap.at(keySpot)->stepJump();
+				}
+					for (int a = 0 + yoff + std::max(yshrink-elevOs, 0) + elevOs; a < pmap.at(keySpot)->height + yoff + elevOs; a++)
+					{
+						for (int l = 0; l < pmap.at(keySpot)->width; l++)
+						{
+							sf::Color col;
+							col.r = 255;
+							col.g = 0;
+							col.b = 0;
+							col.a = 255;
+							std::string thisKeySpot = "" + std::to_string(floorX + l) + ',' + std::to_string(floorY - a);
+							objs::PlayerPixel pp(col, (int)(pmap.at(keySpot)->x), (int)(pmap.at(keySpot)->y));
+							screenumap[thisKeySpot] = pp;
+						}
+					}
+				
+			}
+			bool justObj = false;
+			if (fomap.find(keySpot) != fomap.end()) {
+				if (std::find(mappedfos.begin(), mappedfos.end(), keySpot) == mappedfos.end()) {
+					mappedfos.push_back(keySpot);
+					justObj = true;
+					objs::FixedObject* fop = &fomap.at(keySpot);
+					int wi = fop->width;
+					int he = fop->height;
+					for (int f = 0; f < he; f++) {
+						for (int o = 0; o < wi; o++) {
+							char t = fop->thing[objs::clamp((f * wi) + o, 0, (wi*he)-2)];
+							if (t != '0') {
+								std::string thisKeySpot = "" + std::to_string(floorX + o-(int)(wi/2)) + ',' + std::to_string(floorY + f - he);
+								objs::ObjectBrick ob(opixref[t], fop->x, fop->y);
+								if (opixmap.find(thisKeySpot) == opixmap.end()) {
+									opixmap[thisKeySpot] = ob;
+								}
+								else {
+									if (opixmap.at(thisKeySpot).oby < fop->y) {
+										opixmap[thisKeySpot] = ob;
+									}
+								}
+							}
+						}
+					}
+				}
+
+			} 
+			bool isObjectPix = false;
+			if (opixmap.find(keySpot) != opixmap.end()) {
+				rect.setFillColor(opixmap.at(keySpot).col);
+				rect.setPosition(sf::Vector2f((i - camX) * ts, (j - camY) * ts));
+				window.draw(rect);
+				isObjectPix = true;
+			}
+			
+			if (screenumap.find(keySpot) != screenumap.end() && justObj == false)
+			{
+				if (opixmap.find(keySpot) == opixmap.end()) {
+					rect.setFillColor(screenumap.at(keySpot).col);
+					rect.setPosition(sf::Vector2f((i - camX) * ts, (j - camY) * ts));
+					window.draw(rect);
+				}
+				else {
+					if (opixmap.at(keySpot).oby < screenumap.at(keySpot).py) {
+						rect.setFillColor(screenumap.at(keySpot).col);
+						rect.setPosition(sf::Vector2f((i - camX) * ts, (j - camY) * ts));
+						window.draw(rect);
+					}
+				}
+			} 
+			else
+			if (worldmap.find(keySpot) != worldmap.end() && justObj == false && isObjectPix == false)
+			{
+				rect.setFillColor(worldmap.at(keySpot).col);
+				rect.setPosition(sf::Vector2f((i - camX) * ts, (j - camY) * ts));
+				window.draw(rect);
+			}
+			else if(justObj == false && isObjectPix == false)
+			{
+				int n2 = std::floor(p.noise(i * 2.1, j * 0.2, 11.01 + (perlz / 500)) * 10);
+				int n3 = std::floor(p.noise(i * 5.1, j * 0.4, 11.01 + (perlz / 300)) * 4);
+				int n2Clamped = (std::min(std::max(n2-n3-4, 0), 8))%4;
+				sf::Color col;
+				col.r = (15 * n2Clamped+1);
+				col.g = (15 * n2Clamped);
+				col.b = (90+(n2Clamped*2));
+				col.a = 255;
+				rect.setFillColor(col);
+				rect.setPosition(sf::Vector2f((i - camX) * ts, (j - camY) * ts));
+				window.draw(rect);
+			}
+		}
+	}
+	perlz++;
+}
+
+void renderMinimap(int width, int x, int y, objs::Player* pla) {
+	int revScale = 10;
+	for (int j = y; j < y + width; j++) {
+		for (int i = x; i < x + width; i++) {
+			int localX = (int)(pla->x + ((i-x) * revScale) - ((width*revScale)/2));
+			int localY = (int)(pla->y + ((j-y) * revScale) - ((width*revScale)/2));
+			std::string keySpot = "" + std::to_string(localX) + ',' + std::to_string(localY);
+			if (worldmap.find(keySpot) != worldmap.end()) {
+				rect.setFillColor(worldmap.at(keySpot).col);
+				rect.setPosition(sf::Vector2f(i*ts, j*ts));
+				window.draw(rect);
+			}
+		}
+	}
+}
+
+void handleEvents(objs::Player* pla) {
+	float movement = 1;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		camX -= movement;
+		pla->move(-movement, 0, &pmap);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		camX += movement;
+		pla->move(movement, 0, &pmap);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		camY += movement;
+		pla->move(0, movement, &pmap);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		camY -= movement;
+		pla->move(0, -movement, &pmap);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+	{
+		if (pla->jump == false) {
+			pla->jump = true;
+		}
+	}
+}
+void generateWorld(std::unordered_map<std::string, objs::ColorBrick>* wmap, perlin p) 
+{
+	int ws = 500;
+	sf::Color col;
+	objs::ColorBrick brick(col, 0);
+	for (int j = -ws; j < ws; j++) 
+	{
+		for (int i = -ws; i < ws; i++) 
+		{
+			double n = p.noise(i * 0.02, j * 0.02, 11.01) * 10;
+			double n2 = p.noise(i * 0.05, j * 0.05, 11.01) * 4;
+			double n3 = p.noise(i * 0.01, j * 0.01, 11.01) * 4;
+			double n4 = p.noise(i * 0.9, j * 0.9, 11.01)/2;
+			double n5 = p.noise(i * 0.009, j * 0.009, 7.01) * 15;
+			double ln = (p.noise(i * 0.02, j * 0.02, 93.0122) * 10) + (n4*2);
+			double nClamped = std::min(std::min(n5+ std::max(n+std::max(n3-2-n2, 0.0), 0.0), 10.0), 4.5) + std::max(n4, 0.0);
+			int floorX = std::floor(i);
+			int floorY = std::floor(j);
+			std::string keySpot = "" + std::to_string(floorX) + ',' + std::to_string(floorY);
+			int sandLvl = 3;
+			float waterLvl = 0.6;
+			
+			if (nClamped > waterLvl && nClamped < sandLvl) {
+				float red = (25 + 25 * nClamped);
+				float green = (25 + 25 * nClamped);
+				float blue = (20 * nClamped);
+				float a = (255);
+				float elev = nClamped;
+				insertIntoWorld(floorX, floorY, red, green, blue, a, elev, wmap, brick);
+			}
+			else if (nClamped > sandLvl) 
+			{
+				if (ln < 4.5) {
+					float red = std::min(25 + 5 * (nClamped * 3), 204.5);
+					float green = std::min(25 + 10 * (nClamped * 3), 204.5);
+					float blue = (2 * (nClamped * 3));
+					float a = (255);
+					float elev = nClamped;
+					insertIntoWorld(floorX, floorY, red, green, blue, a, elev, wmap, brick);
+				}
+				else {
+					float red = (25 + 17 * nClamped + (n4 * 100));
+					float green = (25 + 10 * nClamped + (n4 * 100));
+					float blue = (std::min(9 * nClamped + (n4 * 30), 50.0));
+					float a = (255);
+					float elev = nClamped;
+					if (std::rand() > 32500) {
+						objs::FixedObject tree;
+						tree.x = floorX;
+						tree.y = floorY;
+						tree.width = 26;
+						tree.height = 25;
+						tree.thing = objs::Tree::makeTree();
+						std::string keySpot = "" + std::to_string(floorX) + ',' + std::to_string(floorY);
+						fomap[keySpot] = tree;
+					}
+					insertIntoWorld(floorX, floorY, red, green, blue, a, elev, wmap, brick);
+
+				}
+				
+			}
+
+		}
+	}
+}
+void insertIntoWorld(int x, int y, float red, float green, float blue, float a, float elevation, std::unordered_map<std::string, objs::ColorBrick>* wmap, objs::ColorBrick brick) {
+	brick.col.r = red;
+	brick.col.g = green;
+	brick.col.b = blue;
+	brick.col.a = a;
+	brick.elevation = elevation;
+	std::string keySpot = "" + std::to_string(x) + ',' + std::to_string(y);
+	wmap->insert_or_assign(keySpot, brick);
+}
